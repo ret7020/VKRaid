e64dd542f176819ff585344b20d3cabe2b4b2b27
ret7020 <55328925+ret7020@users.noreply.github.com>
2021-08-01 23:33:21 +0300

    Inital commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+import vk_api
+import random
+import time
+from threading import Thread
+
+vk = vk_api.VkApi(token="API ТОКЕН")
+
+vk._auth_token()
+
+vk.get_api()
+
+DEBUG = True  # Enable or disable printing debug information to terminal
+MESSAGES_DELAY = 0.05  # Time to wait after spam message sent
+START_RAID_AFTER_CERTAIN_MESSAGE = False  # Start raid only after sending a message like "@bot_nick start" to chat
+
+longpoll = VkBotLongPoll(vk, ID_группы)
+
+
+def raid(chat_id):
+    if DEBUG: print("New Chat:", chat_id)
+    while True:
+        vk.method("messages.send", {"peer_id": event.object.peer_id,
+                                    "message": "😀 😃 😄 😁 😆 😅 😂 🤣 😇 😉 😊 🙂 🙃 ☺ 😋 😌 😍 🥰 😘 😗 😙 😚 🥲 🤪 😜 😝 😛 🤑 😎 🤓 🥸 🧐 🤠 🥳 🤗 🤡 😏 😶 😐 😑 😒 🙄 🤨 🤔 🤫 🤭 🤥 😳 😞 😟 😠 😡 🤬 😔 😕 🙁 ☹ 😬 🥺 😣 😖 😫 😩 🥱 😤 😮‍💨 😮 😱 😨 😰 😯 😦 😧 😢 😥 😪 🤤 😓 😭 🤩 😵 😵‍💫 🥴 😲 🤯 🤐 😷 🤕 🤒 🤮 🤢 🤧 🥵 🥶 😶‍🌫️ 😴 💤 😈 👿 👹 👺 💩 👻 💀 ☠ 👽 🤖 🎃 😺 😸 😹 😻 😼 😽 🙀 😿 😾 👐 🤲 🙌 👏 🙏 🤝 👍 👎 👊 ✊ 🤛 🤜 🤞 ✌ 🤘 🤟 👌 🤌 🤏 👈 👉 👆 👇 ☝ ✋ 🤚 🖐 🖖 👋 🤙 💪 🦾 🖕 ✍ 🤳 💅 🦵 🦿 🦶 👄 🦷 👅 👂 🦻 👃 👁 👀 🧠 🫀 🫁 🦴 👤 👥 🗣 🫂 👶 👧 🧒 👦 👩 🧑 👨 👩‍🦱 🧑‍🦱 👨‍🦱 👩‍🦰 🧑‍🦰 👨‍🦰 👱‍♀️ 👱 " + str(
+                                        random.randint(0, 163664527287)),
+                                    "random_id": 0})
+        time.sleep(MESSAGES_DELAY)
+
+
+if DEBUG: print("Bot Started")
+while True:
+    for event in longpoll.listen():
+
+        if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.object.peer_id != event.object.from_id:
+                if START_RAID_AFTER_CERATIN_MESSAGE and "start" in event.object.text:
+                    raid(event.object.peer_id)
+                else:
+                    raid(event.object.peer_id)
